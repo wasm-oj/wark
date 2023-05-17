c1a03f8d40a4cd5742764cc181f4e064bb25193c
JacobLinCool <jacoblincool@gmail.com>
2023-05-17 23:46:29 +0800

    docs: add docstring to configs

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+/// Fetches the maximum computational cost limit from the environment variable "MAX_COST".
+/// If the variable is not set or its value cannot be parsed into u64, a default value of 1,000,000,000 is returned.
 pub fn max_cost() -> u64 {
     env::var("MAX_COST")
         .unwrap_or("1000000000".to_owned())
@@ -7,6 +9,8 @@ pub fn max_cost() -> u64 {
         .unwrap_or(1000000000)
 }
 
+/// Fetches the maximum memory limit from the environment variable "MAX_MEMORY".
+/// If the variable is not set or its value cannot be parsed into u32, a default value of 4096 is returned.
 pub fn max_memory() -> u32 {
     env::var("MAX_MEMORY")
         .unwrap_or("4096".to_owned())
@@ -14,6 +18,8 @@ pub fn max_memory() -> u32 {
         .unwrap_or(4096)
 }
 
+/// Fetches the server port number from the environment variable "PORT".
+/// If the variable is not set or its value cannot be parsed into u16, a default value of 33000 is returned.
 pub fn server_port() -> u16 {
     env::var("PORT")
         .unwrap_or("33000".to_owned())
@@ -21,6 +27,8 @@ pub fn server_port() -> u16 {
         .unwrap_or(33000)
 }
 
+/// Fetches the application secret from the environment variable "APP_SECRET".
+/// If the variable is not set, a default value of "APP_SECRET" is returned.
 pub fn app_secret() -> String {
     env::var("APP_SECRET").unwrap_or("APP_SECRET".to_owned())
 }
--- src/read.rs
+++ src/read.rs
@@ -1,6 +1,7 @@
 use std::io::Read;
 use std::{borrow::Cow, fs::File, path::PathBuf};
 
+/// Read a wasm module from a file
 pub fn read_wasm(path: PathBuf) -> Result<Cow<'static, [u8]>, ()> {
     let mut file = File::open(path).expect("wasm module not found");
     let mut wasm_bytes = Vec::new();
